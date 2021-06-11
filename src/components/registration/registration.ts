1fc1684b03c3e314abb568895c3e04acdf3bc0d2
yuriy17 <yuriy_sh@zoho.com>
2021-06-11 16:37:09 +0300

    add regexp

--- src/components/registration/registration.ts
+++ src/components/registration/registration.ts
@@ -1,15 +1,20 @@
-// import '@popperjs/core/dist/umd/popper.min';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// import Popper from '@popperjs/core';
-// import { createPopperLite as createPopper } from '@popperjs/core';
+/* eslint-disable no-control-regex */
+
 import { BootstrapType } from '../../utils/constant';
 import { createElement } from '../../utils/utils';
 import BootstrapComponent from '../bootstrap-component/bootstrap-component';
 import Modal from '../modal/modal';
 import './registration.scss';
 
+const patterns = {
+  username: /^[a-zA-Z0-9][^0-9]([._-](?![._-])|[a-zA-Z0-9]){3,25}[a-zA-Z0-9][^~!@#$%*()_—+=|:;"'`<>,.?/^\0-\cZ]+$/i,
+  surname: /^[a-zA-Z0-9][^0-9]([._-](?![._-])|[a-zA-Z0-9]){3,25}[a-zA-Z0-9][^~!@#$%*()_—+=|:;"'`<>,.?/^\0-\cZ]+$/i,
+  // eslint-disable-next-line max-len
+  email: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+};
+
 export default class Registration {
-  readonly modal:Modal;
+  readonly modal: Modal;
 
   readonly button: HTMLElement;
 
@@ -23,34 +28,40 @@ export default class Registration {
     this.button = this.createLink(this.id);
   }
 
+  validate = (input: HTMLElement, regex: RegExp, errorMessage: string) => {
+
+  };
+
   createModal = (): {
-    id: string,
+    id: string;
     body: {
-      elements: Array<HTMLElement>,
-    },
+      elements: Array<HTMLElement>;
+    };
     header?: {
-      title: string,
-    },
+      title: string;
+    };
     footer?: {
-      isCenter: boolean,
-      elements: Array<HTMLElement>,
-    },
-    isLink?: boolean,
-    isForm?: boolean
+      isCenter: boolean;
+      elements: Array<HTMLElement>;
+    };
+    isLink?: boolean;
+    isForm?: boolean;
   } => {
     const col1 = createElement('div', ['col-12', 'col-md-6']);
     const col2 = createElement('div', ['col-12', 'col-md-6']);
-    const buttonSubmit = createElement('button',
+    const buttonSubmit = createElement(
+      'button',
       ['btn-primary', 'btn'],
-      [
-        ['type', 'submit'],
-      ]);
-    const buttonCancel = createElement('button',
+      [['type', 'submit']],
+    );
+    const buttonCancel = createElement(
+      'button',
       ['btn-secondary', 'btn'],
       [
         ['type', 'button'],
         ['data-bs-dismiss', 'modal'],
-      ]);
+      ],
+    );
     const inputName = new BootstrapComponent({
       type: BootstrapType.input,
       typeInput: 'text',
@@ -77,15 +88,27 @@ export default class Registration {
     });
     const avatar = createElement('div', ['avatar']);
     const avatarBox = createElement('div', ['avatar__box']);
-    const avatarImageLabel = createElement('label', ['avatar__image'], [['for', 'avatarImage']]);
-    const avatarImage = createElement('img', [], [
-      ['src', 'images/avatar.png'],
-      ['alt', 'avatar placeholder'],
-    ]);
-    const avatarImageHover = createElement('img', ['avatar__hover'], [
-      ['src', 'images/avatar-hover.png'],
-      ['alt', 'avatar placeholder hover'],
-    ]);
+    const avatarImageLabel = createElement(
+      'label',
+      ['avatar__image'],
+      [['for', 'avatarImage']],
+    );
+    const avatarImage = createElement(
+      'img',
+      [],
+      [
+        ['src', 'images/avatar.png'],
+        ['alt', 'avatar placeholder'],
+      ],
+    );
+    const avatarImageHover = createElement(
+      'img',
+      ['avatar__hover'],
+      [
+        ['src', 'images/avatar-hover.png'],
+        ['alt', 'avatar placeholder hover'],
+      ],
+    );
     avatarImageLabel.append(avatarImage);
     avatarBox.append(avatarImageLabel, avatarImageHover);
     avatar.append(avatarBox);
